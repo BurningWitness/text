565a502238657a83002bbcf52592a3ee12779416
BurningWitness <frozenwitness@gmail.com>
2023-09-03 13:33:45 +0700

    Including surrogate handling in the encoding algorithm

--- src/Codec/Encoding/UTF8.hs
+++ src/Codec/Encoding/UTF8.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE BinaryLiterals
+           , ViewPatterns #-}
 
 module Codec.Encoding.UTF8 where
 
@@ -12,6 +13,8 @@ data UTF8 = UTF8_1 Word8
           | UTF8_2 Word8 Word8
           | UTF8_3 Word8 Word8 Word8
           | UTF8_4 Word8 Word8 Word8 Word8
+          | Surrogate
+            deriving Show
 
 {-# INLINE utf8 #-}
 utf8 :: Char -> UTF8
@@ -21,9 +24,11 @@ utf8 (ord -> i)
   | i < 0x800     = UTF8_2 (0xC0 + fromIntegral ( i `unsafeShiftR` 6))
                            (0x80 + fromIntegral ( i .&. 0x3F))
 
-  | i <= 0xFFFF   = UTF8_3 (0xE0 + fromIntegral ( i `unsafeShiftR` 12))
-                           (0x80 + fromIntegral ((i `unsafeShiftR` 6)  .&. 0x3F))
-                           (0x80 + fromIntegral ( i                    .&. 0x3F))
+  | i <= 0xFFFF   = if i `unsafeShiftR` 11 == 0b11011
+                      then Surrogate
+                      else UTF8_3 (0xE0 + fromIntegral ( i `unsafeShiftR` 12))
+                                  (0x80 + fromIntegral ((i `unsafeShiftR` 6)  .&. 0x3F))
+                                  (0x80 + fromIntegral ( i                    .&. 0x3F))
 
   | i <= 0x10FFFF = UTF8_4 (0xF0 + fromIntegral ( i `unsafeShiftR` 18))
                            (0x80 + fromIntegral ((i `unsafeShiftR` 12) .&. 0x3F))
